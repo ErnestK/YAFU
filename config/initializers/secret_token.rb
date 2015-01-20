a7e4e62b57c43c474ed3148e2312068120f38c47
unknown <ES_Khasanzhinov@ES017.msk.ottogroupru.local>
2015-01-20 17:14:21 +0300

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YAFU::Application.config.secret_key_base = 'f2b22b5346b16e25ee6e1d5c3a479bccbe06ba1626e673226c6747104d245335cfe0198f6150d52a01336e06852edaf6a09fb0e1f6877511629dec42f10b18bf'
