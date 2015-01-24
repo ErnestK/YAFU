bdab4a798c1aaeaa55edabe8276016aeba61f73a
ErnestK <khasanzhinov@gmail.com>
2015-01-25 02:33:36 +0600

    Add rails-def authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,18 @@ class SessionsController < ApplicationController
 	user = User.find_by(name: params[:name])
 	if user && user.authenticate(params[:password])
 		session[:user_id] = user.id
-		redirect_to admin_url
+		if user.login == 'God'
+		  redirect_to admin_url
+		else
+		  redirect_to scrubs_url
+		end
 	else
 		redirect_to login_url, alert: "Invalid user/password combinations"
 	end	
   end
   
   def destroy
+    session[:user_id] = nil
+    redirect_to login_url, notice: "You logged out."
   end
 end
